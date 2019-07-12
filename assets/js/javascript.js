6a6c9e8cc38f5595bab0fa9c8fb0518fa524ee52
adrianctwo <adrian.e.situ@gmail.com>
2019-07-12 13:19:06 -0700

    Updated strcuture to setup firebase

---
+++ assets/js/javascript.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyBnYa7Q9rt6wLmgNPVyBJdXOvO5h5FeoNc",
+    authDomain: "train-scheduler071219.firebaseapp.com",
+    databaseURL: "https://train-scheduler071219.firebaseio.com",
+    projectId: "train-scheduler071219",
+    storageBucket: "",
+    messagingSenderId: "775605965388",
+    appId: "1:775605965388:web:45a751b57675e47e"
+};
+firebase.initializeApp(config);
